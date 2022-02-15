b73f04808c1e056d5bc5d17340e1397047358e5e
Corrado.Ferra <corradoferra@gmail.com>
2022-02-15 16:56:27 +0100

    1-2-4 fatti; 3 work in progress

--- snack4.php
+++ snack4.php
@@ -1,11 +1,12 @@
-<?php 
-    $numeri = [];
+<?php
+$numeri = [];
 
-    for($i = 0; $i < 15;){
-        $numeroRndm = rand(1,100);
-        if(!in_array($numeroRndm, $numeri)){
-            $numeri[] = $numeroRndm;
-            $i++;
-        }
-    };
-    echo($numeri)
+for ($i = 0; $i < 15;) {
+    $numeroRndm = rand(1, 100);
+    if (!in_array($numeroRndm, $numeri)) {
+        $numeri[] = $numeroRndm;
+        $i++;
+    }
+};
+
+var_dump($numeri);
